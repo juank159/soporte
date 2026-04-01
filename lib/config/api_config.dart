6ae15b56a6cf3435c7bd14c1824a68b9c7ac2c10
juank <juankpaez31@gmail.com>
2026-04-01 11:37:27 -0500

    Add version comment

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -2,3 +2,4 @@ class ApiConfig {
   static const String baseUrl = 'https://soporte-api.baudity.tech/api';
   static const String statusPageUrl = 'https://soporte-api.baudity.tech';
 }
+// App version 1.0.0 - Production build
